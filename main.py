5154d110c1424e42c3b06ef1fa8caa9f8868dc52
Robert Zavala <rbrtzavala@gmail.com>
2025-02-14 11:52:35 -0700

    initialize constants

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
 
 def main():
-    print(f"Starting asteroids!")
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
